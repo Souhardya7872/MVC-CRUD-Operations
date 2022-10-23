fefa741eea482e8e0abf3ceb6d702e0604de98c1
unknown <souhardyapaul7872@gmail.com>
2022-10-24 00:11:37 +0530

    Commit

---
+++ Assessment/Migrations/202210220422310_EmployeeTableCreated.cs
@@ -0,0 +1,29 @@
+﻿namespace Assessment.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class EmployeeTableCreated : DbMigration
+    {
+        public override void Up()
+        {
+            CreateTable(
+                "dbo.Employees",
+                c => new
+                    {
+                        EmployeeId = c.Int(nullable: false, identity: true),
+                        Name = c.String(nullable: false),
+                        Email = c.String(nullable: false),
+                        Address = c.String(nullable: false),
+                        Phone = c.Int(nullable: false),
+                    })
+                .PrimaryKey(t => t.EmployeeId);
+            
+        }
+        
+        public override void Down()
+        {
+            DropTable("dbo.Employees");
+        }
+    }
+}
---
+++ Assessment/Models/Employee.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Assessment.Models
+{
+    public class Employee
+    {
+        
+            [Key]
+            public int EmployeeId { get; set; }
+
+            [Required(ErrorMessage = "Name Required")]
+            [DisplayName("Name")]
+            public string Name { get; set; }
+
+            [Required(ErrorMessage = "Email Required")]
+            [DisplayName("Email")]
+            public string Email { get; set; }
+
+            [Required(ErrorMessage = "Address Required")]
+            [DisplayName("Address")]
+            public string Address { get; set; }
+
+            [Required(ErrorMessage = "Phone Required")]
+            [DisplayName("Phone")]
+            
+            public int Phone { get; set; }
+
+
+        }
+
+        public class EmployeeEntities : DbContext
+        {
+            public DbSet<Employee> employees { get; set; }
+        }
+    
+}
